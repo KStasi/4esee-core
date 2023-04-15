83c41a62b613f18774914ff260ecd3f2ee441355
KStasi <nastasiakondaurova@gmail.com>
2023-04-16 01:45:05 +0300

    Upgrade oracle smart contract

--- src/Oracle.ts
+++ src/Oracle.ts
@@ -4,18 +4,35 @@ import {
   state,
   State,
   method,
-  UInt32,
   PublicKey,
-  Bool,
-  PrivateKey,
-  AccountUpdate,
-  UInt64,
-  Permissions,
-  Circuit,
+  Signature,
 } from 'snarkyjs';
 
 export class Oracle extends SmartContract {
+  @state(PublicKey) oraclePublicKey = State<PublicKey>();
+  @state(Field) result = State<Field>();
+
+  @method initState(oraclePk: PublicKey) {
+    this.oraclePublicKey.set(oraclePk);
+    this.requireSignature();
+  }
+
   @method getResult(): Field {
-    return new Field(1);
+    const result = this.result.get();
+    this.result.assertEquals(result);
+    return result;
+  }
+
+  @method verify(result: Field, signature: Signature) {
+    // Get the oracle public key from the contract state
+    const oraclePublicKey = this.oraclePublicKey.get();
+    this.oraclePublicKey.assertEquals(oraclePublicKey);
+    // Evaluate whether the signature is valid for the provided data
+    const validSignature = signature.verify(oraclePublicKey, [result]);
+    // Check that the signature is valid
+    validSignature.assertTrue();
+    // Check that the provided value represents either true or false
+    result.assertLessThan(Field(3));
+    this.result.set(result);
   }
 }
